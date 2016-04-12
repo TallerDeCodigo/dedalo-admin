588b2d3ec140670092e8cbc9a4c4c28e9202fe01
Pablo Covarrubias <cova@covapro.local>
2016-04-12 18:10:35 -0500

    index

--- themes/admin_dedalo/header.php
+++ themes/admin_dedalo/header.php
@@ -7,6 +7,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="cleartype" content="on">
+		<link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,500italic,700,700italic,300italic,300' rel='stylesheet' type='text/css'>
 		<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 		<?php wp_head(); ?>
 	</head>
@@ -16,3 +17,14 @@
 			<p class="chromeframe">Estás usando una versión <strong>vieja</strong> de tu explorador. Por favor <a href="http://browsehappy.com/" target="_blank"> actualiza tu explorador</a> para tener una experiencia completa.</p>
 		<![endif]-->
 		<div class="container">
+			<div class="header clearfix">
+				<div class="contenedor clearfix">
+					<div class="logo">
+						<a href="<?php echo bloginfo('url'); ?>"><h1>DEDALO</h1></a>
+					</div><!-- logo -->
+					<div class="login">
+						<a href="">LOGIN</a><a class="profilepic" href=""><img src="<?php echo THEMEPATH; ?>/images/profilepic.png"></a>
+					</div><!-- login -->
+				</div><!-- contenedor -->
+			</div><!--header-->
+			
--- themes/admin_dedalo/index.php
+++ themes/admin_dedalo/index.php
@@ -1,6 +1,140 @@
 	<?php get_header(); ?>
 	
-	<a href="<?php echo site_url(); ?>/wp-admin"><img src="<?php echo THEMEPATH;?>/images/logo.png"></a>
+	<div class="search">
+		<form id="searchform" role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+		    <label>
+		        <span class="screen-reader-text"><?php echo _x( '', 'label' ) ?></span>
+		        <input type="text" class="search-field"
+		            placeholder="<?php echo esc_attr_x( 'Buscar...', 'placeholder' ) ?>"
+		            value="<?php echo get_search_query() ?>" name="s"
+		            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+		    </label>
+		</form>
+	</div>
+		
+		<div class="contenedor clearfix">
+		<h3>DESTACADOS</h3>
+		<section class="destacados seccion_home clearfix grid">
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold.png"></a>
+				<h4><a href="">Nombre producto</a></h4>
+				<h5><a href="">Designer/brand</a></h5>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+				<span class="precio">$1,500.00</span>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold.png"></a>
+				<h4><a href="">Nombre producto</a></h4>
+				<h5><a href="">Designer/brand</a></h5>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+				<span class="precio">$1,500.00</span>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold.png"></a>
+				<h4><a href="">Nombre producto</a></h4>
+				<h5><a href="">Designer/brand</a></h5>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+				<span class="precio">$1,500.00</span>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold.png"></a>
+				<h4><a href="">Nombre producto</a></h4>
+				<h5><a href="">Designer/brand</a></h5>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+				<span class="precio">$1,500.00</span>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold.png"></a>
+				<h4><a href="">Nombre producto</a></h4>
+				<h5><a href="">Designer/brand</a></h5>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+				<span class="precio">$1,500.00</span>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold.png"></a>
+				<h4><a href="">Nombre producto</a></h4>
+				<h5><a href="">Designer/brand</a></h5>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+				<span class="precio">$1,500.00</span>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold.png"></a>
+				<h4><a href="">Nombre producto</a></h4>
+				<h5><a href="">Designer/brand</a></h5>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+				<span class="precio">$1,500.00</span>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold.png"></a>
+				<h4><a href="">Nombre producto</a></h4>
+				<h5><a href="">Designer/brand</a></h5>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+				<span class="precio">$1,500.00</span>
+			</article>
+		</section>
+		<h3>NOTICIAS</h3>
+		<section class="notas seccion_home 	clearfix grid">
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold2.png"></a>
+				<h4><a href="">Título</a></h4>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold2.png"></a>
+				<h4><a href="">Título</a></h4>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold2.png"></a>
+				<h4><a href="">Título</a></h4>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold2.png"></a>
+				<h4><a href="">Título</a></h4>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold2.png"></a>
+				<h4><a href="">Título</a></h4>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold2.png"></a>
+				<h4><a href="">Título</a></h4>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold2.png"></a>
+				<h4><a href="">Título</a></h4>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/placehold2.png"></a>
+				<h4><a href="">Título</a></h4>
+				<p class="excerpt">Lorem ipsum dolor sit amet</p>
+			</article>
+		</section><!-- notas -->
+		<h3>MARCAS</h3>
+		<section class="notas seccion_home clearfix grid">
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/brand.png"></a>
+				<h4><a href="">Nombre marca</a></h4>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/brand.png"></a>
+				<h4><a href="">Nombre marca</a></h4>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/brand.png"></a>
+				<h4><a href="">Nombre marca</a></h4>
+			</article>
+			<article class="producto clearfix">
+				<a href=""><img src="<?php echo THEMEPATH; ?>/images/brand.png"></a>
+				<h4><a href="">Nombre marca</a></h4>
+			</article>
+		</section><!-- notas -->
+	</div>
 
 	
 	<?php get_sidebar(); ?>
