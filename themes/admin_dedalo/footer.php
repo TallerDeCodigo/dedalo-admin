94c49d640b73fad05677b8deecd8b20cf0755a21
Pablo Covarrubias <cova@covapro.local>
2016-04-18 12:30:42 -0500

    footer

--- themes/admin_dedalo/footer.php
+++ themes/admin_dedalo/footer.php
@@ -1,5 +1,43 @@
 	
-	<footer></footer>
+	<footer>
+		<div class="contenedor clearfix">
+			<div class="clearfix top_footer">
+				<section class="foot_section">
+					<a href="<?php echo bloginfo('url'); ?>"><img src="<?php echo THEMEPATH; ?>/images/monograma.png"></a>
+				</section>
+				<section class="foot_section">
+					<span>App</span>
+					<a href="">Android</a>
+					<a href="">iOS</a>
+				</section>
+				<section class="foot_section">
+					<span>About 3Dedalo</span>
+					<a href="">Who we are</a>
+					<a href="">Contact</a>
+				</section>
+				<section class="foot_section">
+					<span>Brands</span>
+					<a href="">Join</a>
+					<a href="">Plans</a>
+				</section>
+				<section class="foot_section">
+					<span>Follow us</span>
+					<a href="">Twitter</a>
+					<a href="">Facebook</a>
+					<a href="">Instagram</a>
+				</section>
+			</div><!-- top_footer -->
+			<div class="clearfix bottom_footer">
+				<span>© 3Dedalo 2016</span>
+				<section class="section_foot_bottom">
+					<a href="">Terms of use</a>
+					<a href="">Privacy statement</a>
+					<a href="">Legal</a>
+				</section>
+			</div>
+			</div><!-- bottom_foot -->
+		</div><!-- contenedor -->
+	</footer>
 		<?php wp_footer(); ?>
 		</div><!-- container -->
 	</body>
