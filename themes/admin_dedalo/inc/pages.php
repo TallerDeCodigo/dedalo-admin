f2068518a856690f001fb60b78a886e3b7febd73
John Falcon <john@tallerdecodigo.com>
2016-06-21 18:47:29 -0500

    Create upload model page

--- themes/admin_dedalo/inc/pages.php
+++ themes/admin_dedalo/inc/pages.php
@@ -8,16 +8,16 @@
 
 
 		// CONTACTO
-		/*if( ! get_page_by_path('faq') ){
+		if( ! get_page_by_path('upload-model') ){
 			$page = array(
 				'post_author' => 1,
 				'post_status' => 'publish',
-				'post_title'  => 'Frequently Asked Questions',
-				'post_name'   => 'faq',
+				'post_title'  => 'Upload',
+				'post_name'   => 'upload-model',
 				'post_type'   => 'page'
 			);
 			wp_insert_post( $page, true );
-		}*/
+		}
 
 
 	});
